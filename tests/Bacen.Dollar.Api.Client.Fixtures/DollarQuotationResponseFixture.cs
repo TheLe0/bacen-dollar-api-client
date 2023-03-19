f7823fd4920f8ed71e5b7e7bb076ffc89f2c7519
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2023-03-19 16:11:58 -0300

    tests: Create response fixtures

---
+++ tests/Bacen.Dollar.Api.Client.Fixtures/DollarQuotationResponseFixture.cs
@@ -0,0 +1,15 @@
+﻿using Bacen.Dollar.Api.Client.Responses;
+using Bogus;
+namespace Bacen.Dollar.Api.Client.Fixtures
+{
+    public class DollarQuotationResponseFixture
+    {
+        public static DollarQuotationResponse AutoGenerate(int numOfRecords)
+        {
+            return new Faker<DollarQuotationResponse>()
+                .RuleFor(u => u.Context, (f) => f.Random.String(10))
+                .RuleFor(u => u.QuotationContent, QuotationResponseContentFixture.AutoGenerate(numOfRecords))
+                .Generate();
+        }
+    }
+}
---
+++ tests/Bacen.Dollar.Api.Client.Fixtures/QuotationResponseContentFixture.cs
@@ -0,0 +1,17 @@
+﻿using Bacen.Dollar.Api.Client.Responses;
+using Bogus;
+
+namespace Bacen.Dollar.Api.Client.Fixtures
+{
+    public class QuotationResponseContentFixture
+    {
+        public static IList<QuotationResponseContent> AutoGenerate(int numOfRecords)
+        {
+            return new Faker<QuotationResponseContent>()
+                .RuleFor(u => u.WithdrawQuote, (f) => f.Random.Decimal(0, 10))
+                .RuleFor(u => u.PurchaseQuote, (f) => f.Random.Decimal(0, 10))
+                .RuleFor(u => u.QuotationDateTime, (f) => f.Date.Past(1).ToString())
+                .Generate(numOfRecords);
+        }
+    }
+}
