f004f530e0b0a68ab71580814e771e1d238d2d98
TheLe0 <leob.tosin@hotmail.com>
2023-03-21 18:59:54 -0300

    feat: Setup and configure the DI package

--- src/Bacen.Dollar.Api.Client.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Bacen.Dollar.Api.Client.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Bacen.Dollar.Api.Client.Common;
+using Bacen.Dollar.Api.Client.Configurations;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bacen.Dollar.Api.Client.DependencyInjection
 {
@@ -6,6 +8,33 @@ namespace Bacen.Dollar.Api.Client.DependencyInjection
     {
         public static IServiceCollection AddBacenDollarApiClient(this IServiceCollection services)
         {
+            services.AddTransient<IBacenDollarHttpClient, BacenDollarHttpClient>();
+
+            services.AddTransient<IBacenDollarClient>(x =>
+                new BacenDollarClient(x.GetRequiredService<IBacenDollarHttpClient>()));
+
+            return services;
+        }
+
+        public static IServiceCollection AddBacenDollarApiClient(this IServiceCollection services, string baseUrl)
+        {
+            services.AddTransient<IBacenDollarHttpClient>(_ =>
+                new BacenDollarHttpClient(baseUrl));
+
+            services.AddTransient<IBacenDollarClient>(x =>
+                new BacenDollarClient(x.GetRequiredService<IBacenDollarHttpClient>()));
+
+            return services;
+        }
+
+        public static IServiceCollection AddBacenDollarApiClient(this IServiceCollection services, BacenDollarClientConfiguration configs)
+        {
+            services.AddTransient<IBacenDollarHttpClient>(_ =>
+                new BacenDollarHttpClient(configs));
+
+            services.AddTransient<IBacenDollarClient>(x =>
+                new BacenDollarClient(x.GetRequiredService<IBacenDollarHttpClient>()));
+
             return services;
         }
     }
