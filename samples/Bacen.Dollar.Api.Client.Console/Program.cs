bf0a8dda6bd744861565fb3001dca4b1fd1eb1da
TheLe0 <leob.tosin@hotmail.com>
2023-03-21 20:05:04 -0300

    feat: Create console sample

--- samples/Bacen.Dollar.Api.Client.Console/Program.cs
+++ samples/Bacen.Dollar.Api.Client.Console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bacen.Dollar.Api.Client;
+using Bacen.Dollar.Api.Client.Configurations;
+
+var baseUrl = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/";
+var configs = new BacenDollarClientConfiguration
+{
+    BaseUrl = baseUrl,
+    ThrowOnAnyError = false,
+    MaxTimeout = 50000
+};
+
+var client = new BacenDollarClient(configs);
+
+var dollarQuotation = await client.DailyDollarQuotationAsync(DateTime.Today)
+    .ConfigureAwait(false);
+
+Console.WriteLine(dollarQuotation);
